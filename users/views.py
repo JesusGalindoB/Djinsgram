ea3df4885e965722197617363462816e9cb696a0
jesus <jesbaga.17@gmail.com>
2020-02-10 07:52:58 -0500

    Add templates and static files

--- users/views.py
+++ users/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return HttpResponse('Hello, world')
+    return render(request, 'users/login.html')
